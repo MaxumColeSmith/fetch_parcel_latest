81f7942de5fdef330cfe0b1fa18fc158e89073b4
Max Smith <maxsmith1200@gmail.com>
2022-03-26 21:56:44 -0700

    remove sample comment

--- index.ts
+++ index.ts
@@ -15,23 +15,11 @@ const process_browser = async ({
 	let driver = builder.forBrowser('chrome').build();
 	// If default count to 1000, that seems to be the max allowable return
 	await driver.get(`https://parcels.lewiscountywa.gov/search?q=${searchPhrase}&page=${pageToStart}&count=${1000}`);
-
-	// the search page returns a table with parcel numbers in the attribute data-href
-
-	// <tr data-href="/029454005000">
-	//         <td>029454005000   </td>
-	//         <td>HUDSON, PATRICIA A</td>
-	//         <td>228 OSBORNE RD A</td>
-	//         <td>MOSSYROCK</td>
-	//         <td>2193982        </td>
-	//         <td>
-	//              <a href="/add-to-cart/029454005000   " class="btn btn-xs btn-primary">
-	//              <i class="glyphicon glyphicon-shopping-cart"></i> pay my taxes</a>
-	//         </td>
-	//     </tr>
+	
 	let assessedValueArray = await driver.findElements(By.xpath('//tr'));
 	let itemsToSave = [];
 	for (let assessedValue of assessedValueArray) {
+		// the search page returns a table with parcel numbers in the attribute data-href
 		const dataHref = await (assessedValue).getAttribute("data-href");
 		itemsToSave.push({
 			parcelNumber: dataHref,
